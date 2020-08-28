16b5aa9251848e14b28cb73bbfdeb56ef58e20c0
odeane_croney <odeane_croney@yahoo.com>
2020-08-28 09:57:39 +0200

    refactor to test mode

---
+++ controllers/booksController.js
@@ -0,0 +1,14 @@
+const models = require('../models')
+
+const booksController = {
+  async index(request, response, next) {
+    const booksIndex = await models.Book.findAll()
+    response.json({books: booksIndex})
+  },
+  async show(request, response, next) {
+    const { id } = request.params
+    const book = await models.Book.findByPk(id)
+    response.send({book:book})
+  }
+}
+module.exports = booksController
